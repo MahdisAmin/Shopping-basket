b84065677773a28b25840d364fe049f133291cbb
Mahdis Amin <mahdis.amin95@gmail.com>
2025-04-22 00:59:41 +0330

    wrap Routes component with ProductsProvider for context management

--- shopping-basket/src/App.jsx
+++ shopping-basket/src/App.jsx
@@ -3,16 +3,19 @@ import ProductsPage from "./pages/ProductsPage";
 import DetailePage from "./pages/DetailePage";
 import Checkout from "./pages/Checkout";
 import PageNotFound from "./pages/404";
+import ProductsProvider from "./context/ProductsContext";
 
 function App() {
   return (
-    <Routes>
-      <Route index element={<Navigate to="/products" />} />
-      <Route path="/products" element={<ProductsPage />} />
-      <Route path="/products/:id" element={<DetailePage />} />
-      <Route path="/checkout" element={<Checkout />} />
-      <Route path="/*" element={<PageNotFound />} />
-    </Routes>
+    <ProductsProvider>
+      <Routes>
+        <Route index element={<Navigate to="/products" />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/products/:id" element={<DetailePage />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/*" element={<PageNotFound />} />
+      </Routes>
+    </ProductsProvider>
   );
 }
 
