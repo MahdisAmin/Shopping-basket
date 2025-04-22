1c56875e5522f340ddeda3fce9467bc9d7280b29
Mahdis Amin <mahdis.amin95@gmail.com>
2025-04-23 03:05:10 +0330

    Add search input and button to ProductsPage component

--- shopping-basket/src/pages/ProductsPage.jsx
+++ shopping-basket/src/pages/ProductsPage.jsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
 import Card from "../components/Card";
 import Loader from "../components/Loader";
 import { useProduct } from "../context/ProductsContext";
+import {ImSearch} from 'react-icons/im'
 
 function ProductsPage() {
   const products = useProduct();
-  console.log(products);
+const [search, setSearch] = useState('')
 
   return (
+    <>
+      <div>
+        <input type="text" placeholder="Search..." value={search} onChange={e=> setSearch(e.target.value.toLocaleLowerCase().trim())} />
+        <button onClick={()=> {console.log("search");
+        }}><ImSearch/></button>
+        
+      </div>
     <div className="flex justify-between">
       <div className="w-full flex flex-wrap justify-between ">
         {!products.length && <Loader />}
@@ -16,6 +25,7 @@ function ProductsPage() {
       </div>
       <div>Sidebar</div>
     </div>
+    </>
   );
 }
 
